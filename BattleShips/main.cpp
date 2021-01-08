3bb6f1bd41d2ed7db5e3bf3c0f8840aa76300d6b
Kaloyan <kaloyan.va@abv.bg>
2021-01-08 11:51:57 +0200

    Initial Commit

---
+++ BattleShips/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BattleShips
+//
+//  Created by Kaloyan Vachkov on 8.01.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
